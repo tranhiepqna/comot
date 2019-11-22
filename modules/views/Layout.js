5fc0900d711778ce2c2c956b3ed56804339eead1
hiep.tran@bigcosmt.com <hiep.tran@bigcosmt.com>
2019-11-22 11:05:12 +0700

    fix about page issue

--- modules/views/Layout.js
+++ modules/views/Layout.js
@@ -9,6 +9,8 @@ export default function Layout(props) {
      <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
+        <meta httpEquiv="content-language" content="vi" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
         <title>{props.siteTitle}</title>
         <meta name="Description" content={props.description}></meta>
         <link rel="canonical" href="https://comot.vn/" />
